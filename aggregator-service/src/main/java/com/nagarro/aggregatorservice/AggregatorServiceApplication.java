f98fe6d7ba6519e34af9a5bb7974a1afde8bdff4
shadabbahadara <shadabbahadara@gmail.com>
2020-06-13 11:25:13 +0530

    BNSEH 3344 added rest template error handling

--- aggregator-service/src/main/java/com/nagarro/aggregatorservice/AggregatorServiceApplication.java
+++ aggregator-service/src/main/java/com/nagarro/aggregatorservice/AggregatorServiceApplication.java
@@ -2,22 +2,9 @@ package com.nagarro.aggregatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class AggregatorServiceApplication {
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-
-	@Bean
-	public WebClient.Builder getWebClientBuilder() {
-		return WebClient.builder();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AggregatorServiceApplication.class, args);
--- aggregator-service/src/main/java/com/nagarro/aggregatorservice/resources/AggregatorResource.java
+++ aggregator-service/src/main/java/com/nagarro/aggregatorservice/resources/AggregatorResource.java
@@ -1,30 +1,39 @@
 package com.nagarro.aggregatorservice.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.nagarro.aggregatorservice.models.OrderDetails;
 import com.nagarro.aggregatorservice.models.OrderList;
 import com.nagarro.aggregatorservice.models.User;
+import com.nagarro.exceptions.RestTemplateResponseErrorHandler;
+
 
 @RestController
 @RequestMapping("/orderdetails")
 public class AggregatorResource {
 
-	@Autowired
+	private static Logger LOG = LoggerFactory.getLogger(AggregatorResource.class);
+
 	private RestTemplate restTemplate;
 
 	@Autowired
-	private WebClient.Builder webClientBuilder;
+	public AggregatorResource(RestTemplateBuilder builder) {
+		restTemplate = builder.errorHandler(new RestTemplateResponseErrorHandler()).build();
+	}
 
 	@GetMapping(path = "/{userId}")
 	public OrderDetails getOrders(@PathVariable("userId") int userId) {
+		LOG.info("calling User service...");
 		User user = restTemplate.getForObject("http://localhost:8081/user/" + userId, User.class);
+		LOG.info("calling Order service...");
 		OrderList orderList = restTemplate.getForObject("http://localhost:8082/orders/" + userId, OrderList.class);
 
 		return new OrderDetails(user, orderList.getOrders());
